5e4b7022a53ab68c2936032ffff4d1dae5d80aba
Stijn Wellink <wellinkstijn@gmail.com>
2026-03-02 09:25:34 +0100

    added footer and KoFi stuff for the footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import s from "./Footer.module.css";
+import Link from "next/link";
+
+export default function Footer() {
+
+    return (
+       <footer>
+            <h1 className={"text-lg-end"}>gay!?</h1>
+       </footer>
+    );
+}
